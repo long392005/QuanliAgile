b79d9a3d14a8a3c47f4e982e95ae64d0a454b0b1
Minhsofeed <phanvietminh23@gmail.com>
2025-04-12 14:01:32 +0700

    Controler chi tieết và cập nhật sản phẩm

--- index.php
+++ index.php
@@ -6,9 +6,10 @@ require_once './commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
 require_once './controllers/HomeController.php';
+require_once './controllers/AdminSanPhamController.php';
 
 // Require toàn bộ file Models
-
+require_once './models/AdminSanPham.php';
 // Route
 $act = $_GET['act'] ?? '/';
 
@@ -16,5 +17,7 @@ $act = $_GET['act'] ?? '/';
 
 match ($act) {
     // Trang chủ
-    '/'                 => (new HomeController())->index(),
+
+    'chi-tiet-san-pham' => (new AdminSanPhamController())->detailSanpham(), 
+    'cap-nhat-san-pham' => (new AdminSanPhamController())->formEditSanpham(),       
 };
